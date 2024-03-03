f068bf77d540aea348e8684d13f57580af41c014
Yaya Zhang <woyaya114@live.com>
2024-03-03 22:25:45 +0100

    main method created, exercises 1 done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Person person1 = new Person("John", "Doe", 30, "123 Main St, Apt 4B, 12345, Exampleville, Countryland", "john.doe@example.com", "(123) 456-7890");
+        
+        person1.PersonInfo();
+
+    }
+}
